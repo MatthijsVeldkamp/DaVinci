6e4fb056c7f7840212d71f545a0ea21bb2dd5b50
Matthijs Veldkamp <99071402@mydavinci.nl>
2022-11-21 13:03:52 +0100

    Create M&M's.py

---
+++ Matthijs/Module 4 - Lijstjes en samenstellingen/Deel 1 - collections/M&M's.py
@@ -0,0 +1,21 @@
+import random
+import time
+import os
+os.system("cls")
+kleuren = ("oranje","blauw","groen","bruin")
+zak = []
+print("Hoeveel M&M's wilt u?")
+try:
+    hoeveelheid = int(input("? "))
+    for i in range(hoeveelheid):
+        zak.append(random.choice(kleuren))
+    zak = str(zak)
+    zak = zak.replace("[", "")
+    zak = zak.replace("]", "")
+    zak = zak.replace("'", "")
+    print("In de zak zitten M&M's met de kleuren: "+(zak))
+except:
+    os.system("cls")
+    print("Vul alleen nummers in!")
+    time.sleep(2)
+    os.system("cls")
