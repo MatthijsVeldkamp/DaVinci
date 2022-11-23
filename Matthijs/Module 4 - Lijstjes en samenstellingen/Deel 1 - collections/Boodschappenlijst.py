ac6bf50734729233577597f3564c5a792328efca
Matthijs Veldkamp <99071402@mydavinci.nl>
2022-11-23 21:18:21 +0100

    Update Boodschappenlijst.py

--- Matthijs/Module 4 - Lijstjes en samenstellingen/Deel 1 - collections/Boodschappenlijst.py
+++ Matthijs/Module 4 - Lijstjes en samenstellingen/Deel 1 - collections/Boodschappenlijst.py
@@ -1,6 +1,7 @@
 import time
 import os
 producten = []
+hoeveelheid = []
 while True:
     os.system("cls")
     product = input("Welk product wilt u toevoegen?\n? ")
@@ -11,12 +12,22 @@ while True:
     else:
         os.system("cls")
         hoeveelheid1 = input("Hoeveel keer?\n? ")
-        producten.append(hoeveelheid1+"x "+(product))
+        producten.append(product)
+        hoeveelheid.append(hoeveelheid1)
         os.system("cls")
         input1 = input("Wilt u nog een item toevoegen?\n? ")
         if input1.lower() == ("n") or input1.lower() == ("nee"):
             os.system("cls")
-            print(producten)
+            producten = str(producten)
+            producten = producten.replace("[", "")
+            producten = producten.replace("]", "")
+            producten = producten.replace("'", "")
+            hoeveelheid = str(hoeveelheid)
+            hoeveelheid = hoeveelheid.replace("[", "")
+            hoeveelheid = hoeveelheid.replace("]", "")
+            hoeveelheid = hoeveelheid.replace("'", "")
+            print("Product: ")
+            print("Hoeveelheid: ")
             break
         else:
             continue
