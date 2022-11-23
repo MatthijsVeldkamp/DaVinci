7a22887a6994002ecc5bd3b170d76df95fdd8bd1
Matthijs Veldkamp <99071402@mydavinci.nl>
2022-11-23 11:10:01 +0100

    a

--- Matthijs/Module 4 - Lijstjes en samenstellingen/Deel 1 - collections/test.py
+++ Matthijs/Module 4 - Lijstjes en samenstellingen/Deel 1 - collections/test.py
@@ -1,32 +1,6 @@
-import os
-import time
-boodschappenlijstje = {
-}
-while True:
-    os.system("cls")
-    product = input("Wat wilt u toevoegen? ")
-    productlower = product.lower()
-    if productlower not in boodschappenlijstje:
-        hoeveel = input("Hoeveel keer? ")
-        boodschappenlijstje[productlower] = hoeveel
-        input1 = input("Wilt u nog wat toevoegen? ")
-        if input1.lower() == ("y") or input1.lower() == ("ja") or input1.lower() == ("yes") or input1.lower() == ("j"):
-            continue
-        elif input1.lower() == ("n") or input1.lower() == ("nee") or input1.lower() == ("no"):
-            os.system("cls")
-            print("Boodschappenlijstje:\n")
-            for key,value in boodschappenlijstje.items():
-                print(key+": "+value+"x")
-            break
-    else:
-        hoeveel = input("Hoeveel keer? ")
-        boodschappenlijstje[productlower] = hoeveel
-        input1 = input("Wilt u nog wat toSevoegen? ")
-        if input1.lower() == ("y") or input1.lower() == ("ja") or input1.lower() == ("yes") or input1.lower() == ("j"):
-            continue
-        elif input1.lower() == ("n") or input1.lower() == ("nee") or input1.lower() == ("no"):
-            os.system("cls")
-            print("Boodschappenlijstje:\n")
-            for key,value in boodschappenlijstje.items():
-                print(key+": "+value+"x")
-            break
+speciaal = ("harten","ruiten","klaveren","schoppen")
+kaarten = ("2","3","4","5","6","7","8","9","10","boer","vrouw","heer","aas")
+hallo = "leuke string"
+# for i in (1, 2, 3, 4):
+#     print(i)
+print (hallo[3])
