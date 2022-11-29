c63f7130af057938e599069a9143f0e9db477cd0
Matthijs Veldkamp <99071402@mydavinci.nl>
2022-11-29 15:20:24 +0100

    Opdracht 11 gefixt
    Opdracht 11 gefixt

--- Matthijs/Module 4 - Lijstjes en samenstellingen/Deel 2 - fruit fruit fruit/Opdracht11.py
+++ Matthijs/Module 4 - Lijstjes en samenstellingen/Deel 2 - fruit fruit fruit/Opdracht11.py
@@ -1,33 +1,35 @@
 from fruitmand import *
 loop = True
 kleuren = []
+naam = []
 lijstkleuren = []
+rond = 0
+nietrond = 0
 for x in range(len(fruitmand)):
     kleur = (fruitmand[x]['color'])
     if kleur not in lijstkleuren:
         lijstkleuren.append(kleur)
-
 while loop == True:
-    kleur = str(input("Kies een kleur uit de kleuren" + str(lijstkleuren)))
+    kleur = str(input("Kies een kleur uit de kleuren" + str(lijstkleuren)+"\n? "))
     if (kleur.lower()) not in lijstkleuren:
         print ("De Kleur " + kleur + " zit er niet in de fruitmand")
         loop = True
     else:
         print("Kleur zit in de lijst.")
         loop = False
-
-    for x in range(len(fruitmand)):
-        kleuren.append(fruitmand[x]["round"])
-    rond = kleuren.count(True)
-    nietrond= kleuren.count(False)
-    som = (rond-nietrond)
-    if som <0:
-        som = abs(som)
-        print("er is",som, "meer niet ronde dan ronde van de kleur",kleur)
-        break
-    elif som >0:
-        print("er is",som, "meer ronde dan niet ronde")
-        break
-    elif som == 0:
-        print("er zijn evenveel ronde als niet ronde")
-        break
+for fruit in fruitmand:
+            if fruit['color'] == kleur:
+                if fruit['round'] == True:
+                    rond += 1
+                else:
+                    nietrond += 1
+print (rond)
+print (nietrond)
+verschil = rond - nietrond
+verschil = abs(verschil)
+if rond > nietrond:
+    print (f"Er zijn {int(verschil)} meer ronde vruchten dan niet ronde vruchten in de kleur {str(kleur)}")
+if rond < nietrond:
+        print (f"Er zijn {str(verschil)} minder ronde vruchten dan niet ronde vruchten in de kleur {str(kleur)}")
+if rond == nietrond:
+    print (f"Er zijn {str(verschil)} ronde vruchten en {str(nietrond)} niet ronde vruchten in de kleur {str(kleur)}")
