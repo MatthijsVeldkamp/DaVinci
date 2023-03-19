13dc1b52bc272c1477911faaefd2c66827d65506
MatthijsVeldkamp <99071402@mydavinci.nl>
2023-03-19 11:30:47 +0100

    test.py

--- Matthijs/Module 5 - Meer Fun(ctions)/DRY ijssalon/test.py
+++ Matthijs/Module 5 - Meer Fun(ctions)/DRY ijssalon/test.py
@@ -1,3 +1,24 @@
-dictionary = {'Aantal bolletjes': 0,
-              'Aantal bakjes': 0,
-              'Aantal hoorntjes': 0}
+aantalbolletjes = 0
+welkesmaken = ["chocola", "aardbei", "munt", "mokka", "walnoot","vanille"]
+smaken = []
+def vraag():
+    try:
+        aantalbolletjes = int(input("Hoeveel bolletjes ijs wilt u? "))
+        return aantalbolletjes
+    except:
+        print("Sorry, dat snap ik niet.")
+        vraag()
+aantalbolletjes = vraag()
+for aantalbolletjes in range(aantalbolletjes):
+    while True:
+        smaak = input(f"Welke smaak wilt u voor bolletje {aantalbolletjes+1}? ").lower()
+        # checken of de smaak in de lijst welkesmaken staat
+        if smaak in welkesmaken:
+            smaken.append(smaak)
+            break
+        else:
+            print("Sorry, die hebben we niet.")
+            aantalbolletjes - 1
+            continue
+for aantalbolletjes in range(aantalbolletjes+1):
+    print(f"bolletje {aantalbolletjes+1}: {smaken[aantalbolletjes]}")
